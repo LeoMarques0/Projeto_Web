e842a3f8decdaa6c8aa93c79d0f4c935d343c772
LeoMarques0 <leo.p.marques@Hotmail.com>
2020-03-24 20:48:32 -0300

    Laser_Start

---
+++ Projeto_Web/Assets/Scripts/Enemy/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public Transform parent;
+    public Vector2 shotDir;
+
+    public float spd;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        transform.up = shotDir;
+        rb.velocity = shotDir * spd;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform != parent)
+            Destroy(gameObject);
+    }
+}
